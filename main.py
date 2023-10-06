c729ee1b2ecae7903da0639735811d2ebff16d77
DeveloperUchida <kouei28poiou@icloud.com>
2023-10-06 10:25:26 +0900

    DataUpdate 2023/10/06

--- main.py
+++ main.py
@@ -1,30 +1,41 @@
 import PySimpleGUI as sg
 import datetime
+
 sg.theme("DarkBrown3")
-layout = [[sg.T("0:00:00.000000",font=("Arial",40), k="text",size=(15,1),justification="center")],
-[sg.Push(),sg.B("START/STOP", k="btn"),sg.Push()]]
-win = sg.Window("ストップウォッチ",layout,font=(None,14),size=(400,120),keep_on_top=True)
 
-def execute():
-    if startflag == True:
+layout = [
+    [sg.Text("0:00:00.000000", font=("Arial", 30), key="text", size=(15, 1), justification="center")],
+    [sg.Push(), sg.Button("START/STOP", key="btn", size=(11, 1)), sg.Push()]
+]
+
+win = sg.Window("ストップウォッチ", layout, font=(None, 14), size=(400, 120), keep_on_top=True)
+
+startflag = False
+start = None
+
+while True:
+    event, values = win.read(timeout=50)
+
+    if event == sg.WIN_CLOSED:
+        break
+
+    if event == "btn":
+        if startflag:
+            startflag = False
+        else:
+            start = datetime.datetime.now()
+            startflag = True
+
+    if startflag:
         now = datetime.datetime.now()
-        td = now - start
-        win["txt"].update(td)
+        elapsed_time = now - start
+        # 経過時間を適切に表示するためにフォーマット
+        formatted_time = "{:02}:{:02}:{:02}.{:06}".format(
+            elapsed_time.seconds // 3600,
+            (elapsed_time.seconds // 60) % 60,
+            elapsed_time.seconds % 60,
+            elapsed_time.microseconds
+        )
+        win["text"].update(formatted_time)
 
-        def startstop():
-            global start,startflag
-            if startflag == True:
-                startflag == False
-            else:
-                start = datetime.datetime.now()
-                startflag = True
-
-                startflag = False
-                while True:
-                    e,v = win.read(timeout=50)
-                    execute()
-                    if e == "btn":
-                        startstop()
-                    if e == None:
-                        break
 win.close()
